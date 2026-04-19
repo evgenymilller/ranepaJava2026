03f0051d92d5adbfbb04a01183bb213218fcfdb1
Evg Miller <em23012301@gmail.com>
2026-04-19 16:20:19 +0300

    Хранилище данных

--- src/main/java/employee/repository/EmployeeRepository.java
+++ src/main/java/employee/repository/EmployeeRepository.java
@@ -1,5 +1,33 @@
 package employee.repository;
 
+import employee.model.Employee;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EmployeeRepository {
-    
+    private final Map<Long, Employee> storage = new HashMap<>();
+    private long currentId = 1;
+
+    public Employee save(Employee employee) {
+        if (employee.getId() == null) {
+            employee.setId(currentId++);
+        }
+        storage.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public List<Employee> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    public Employee findById(Long id) {
+        return storage.get(id);
+    }
+
+    public boolean delete(Long id) {
+        return storage.remove(id) != null;
+    }
 }
